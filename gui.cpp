e2dff5853d4a099ccd71286a2b965599d7267389
Tolga Uçar <freetolga@protonmail.com>
2026-06-06 14:04:31 +0000

    add some sliders for adjusting tones of the message being modulated

--- gui.cpp
+++ gui.cpp
@@ -1,8 +1,8 @@
-#include "wx/brush.h"
-#include "wx/event.h"
-#include "wx/gdicmn.h"
-#include "wx/print.h"
-#include "wx/sizer.h"
+#include <wx/brush.h>
+#include <wx/event.h>
+#include <wx/gdicmn.h>
+#include <wx/print.h>
+#include <wx/sizer.h>
 #include <valarray>
 #include <wx/wx.h>
 //#include <matplot/matplot.h>
@@ -28,14 +28,23 @@ private:
     void OnAbout(wxCommandEvent& event);
     void OnPlot(wxCommandEvent& event);
     void OnResize(wxSizeEvent& event);
-    mpWindow * m_plot = new mpWindow(this, -1, wxDefaultPosition, wxSize(200, 200), wxSUNKEN_BORDER);
-    mpWindow * f_plot = new mpWindow(this, -1, wxDefaultPosition, wxSize(200, 200), wxSUNKEN_BORDER);
+    void OnSliderChange(wxScrollEvent& event);
+    mpWindow *m_plot = new mpWindow(this, -1, wxDefaultPosition);
+    mpWindow *f_plot = new mpWindow(this, -1, wxDefaultPosition);
+    wxSlider *fm1_slider, *fm2_slider, *fm3_slider;
+    double fm1 = 20;
+    double fm2 = 200;
+    double fm3 = 2000;
+    double fc = 1000;
 };
 
 enum
 {
     ID_Hello = 1,
     BUTTON_plot = wxID_HIGHEST + 1,
+    SLIDER_FM1,
+    SLIDER_FM2,
+    SLIDER_FM3
 };
 
 wxIMPLEMENT_APP(MyApp);
@@ -48,18 +57,24 @@ bool MyApp::OnInit()
 }
 
 MyFrame::MyFrame()
-    : wxFrame(NULL, wxID_ANY, "Hello World")
+    : wxFrame(NULL, wxID_ANY, "FM Demo")
 {
     wxBoxSizer *main_sizer = new wxBoxSizer(wxHORIZONTAL);
-    wxGridSizer *plots_sizer = new wxGridSizer(2,2, 10 ,10);
-    wxBoxSizer *buttons_sizer = new wxBoxSizer(wxVERTICAL);
+    wxGridSizer *plots_sizer = new wxGridSizer(2, 2, 0 ,0);
+    wxBoxSizer *controls_sizer = new wxBoxSizer(wxVERTICAL);
 
-    wxButton *plotButton = new wxButton(this, BUTTON_plot, "Hello World", wxDefaultPosition, wxSize(100,60));
-    buttons_sizer->Add(plotButton, 0, wxSHAPED, 0);
-    plots_sizer->Add(m_plot, 1, wxSHAPED, 10);
-    plots_sizer->Add(f_plot, 1, wxSHAPED, 10);
-    main_sizer->Add(buttons_sizer, 1, wxEXPAND, 10);
-    main_sizer->Add(plots_sizer, 9, wxEXPAND, 10);
+    wxButton *plotButton = new wxButton(this, BUTTON_plot, "Generate and Plot");
+    this->fm1_slider = new wxSlider(this, SLIDER_FM1, 20, 0, 2000);
+    this->fm2_slider = new wxSlider(this, SLIDER_FM2, 20, 0, 2000);
+    this->fm3_slider = new wxSlider(this, SLIDER_FM3, 20, 0, 2000);
+    controls_sizer->Add(plotButton, 0, wxSHAPED, 0);
+    controls_sizer->Add(fm1_slider, 0, wxSHAPED, 0);
+    controls_sizer->Add(fm2_slider, 0, wxSHAPED, 0);
+    controls_sizer->Add(fm3_slider, 0, wxSHAPED, 0);
+    plots_sizer->Add(m_plot, 1, wxEXPAND, 0);
+    plots_sizer->Add(f_plot, 1, wxEXPAND, 0);
+    main_sizer->Add(controls_sizer, 2, wxEXPAND, 0);
+    main_sizer->Add(plots_sizer, 10, wxEXPAND, 0);
     SetSizer(main_sizer); 
     SetAutoLayout(TRUE);
 
@@ -87,10 +102,6 @@ void MyFrame::OnHello(wxCommandEvent& event)
 }
 
 void MyFrame::OnPlot(wxCommandEvent& event) {
-    double fm1 = 20;
-    double fm2 = 200;
-    double fm3 = 220;
-    double fc = 1000;
     std::valarray<double> time;
     double t1 = 0;
     double t2 = 0.1;
@@ -100,7 +111,7 @@ void MyFrame::OnPlot(wxCommandEvent& event) {
     for(int i = 0; i < size; ++i) {
         time[i] = dt*i;
     }
-    message_signal m1(fm1, fm2, fm3, fc, time);
+    message_signal m1(this->fm1, this->fm2, this->fm3, this->fc, time);
     std::vector<double> x;
     std::vector<double> y;
     for(double d: m1.time) {
@@ -113,7 +124,7 @@ void MyFrame::OnPlot(wxCommandEvent& event) {
     vectorLayer1->SetData(std::move(x), std::move(y));
     vectorLayer1->SetContinuity(true);
     vectorLayer1->SetPen(wxPen(*wxBLUE, 2, wxPENSTYLE_SOLID));
-    vectorLayer1->SetDrawOutsideMargins(false);
+    vectorLayer1->SetDrawOutsideMargins(true);
     m_plot->DelLayer(vectorLayer1);
     m_plot->AddLayer(vectorLayer1);
     m_plot->Fit();
@@ -132,7 +143,7 @@ void MyFrame::OnPlot(wxCommandEvent& event) {
     vectorLayer2->SetData(std::move(x2), std::move(y2));
     vectorLayer2->SetContinuity(true);
     vectorLayer2->SetPen(wxPen(*wxBLUE, 2, wxPENSTYLE_SOLID));
-    vectorLayer2->SetDrawOutsideMargins(false);
+    vectorLayer2->SetDrawOutsideMargins(true);
     f_plot->DelLayer(vectorLayer2);
     f_plot->AddLayer(vectorLayer2);
     f_plot->Fit();
@@ -140,6 +151,21 @@ void MyFrame::OnPlot(wxCommandEvent& event) {
 
 void MyFrame::OnResize(wxSizeEvent& event) {
     this->Refresh();
-    m_plot->Fit();
-    f_plot->Fit();
+    m_plot->UpdateAll();
+    f_plot->UpdateAll();
+}
+
+void MyFrame::OnSliderChange(wxScrollEvent& event) {
+    switch(event.GetId()) {
+        case SLIDER_FM1:
+            fm1 = this->fm1_slider->GetValue();
+            break;
+        case SLIDER_FM2:
+            fm2 = this->fm2_slider->GetValue();
+            break;
+        case SLIDER_FM3:
+            fm3 = this->fm3_slider->GetValue();
+            break;
+
+    }
 }
